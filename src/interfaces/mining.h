5d8fe1e80f22b8cdac3bce18717d14af457d3694
Sjors Provoost <sjors@sprovoost.nl>
2024-05-30 17:06:59 +0200

    rpc: call processNewBlock via miner interface

--- src/interfaces/mining.h
+++ src/interfaces/mining.h
@@ -30,6 +30,15 @@ public:
     //! Returns the hash for the tip of this chain, 0 if none
     virtual uint256 getTipHash() = 0;
 
+    /**
+     * Processes new block. A valid new block is automatically relayed to peers.
+     *
+     * @param[in]   block The block we want to process.
+     * @param[out]  new_block A boolean which is set to indicate if the block was first received via this call
+     * @returns     If the block was processed, independently of block validity
+     */
+    virtual bool processNewBlock(const std::shared_ptr<const CBlock>& block, bool* new_block) = 0;
+
     //! Return the number of transaction updates in the mempool,
     //! used to decide whether to make a new block template.
     virtual unsigned int getTransactionsUpdated() = 0;
